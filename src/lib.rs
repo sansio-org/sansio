57b0d1573b42cea82b30422d2c356d60727e0e14
yngrtc <y@ngr.tc>
2025-11-07 15:01:47 -0800

    use Associated Error type

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,35 @@
-use std::io::Result;
 use std::time::Instant;
 
 pub trait Handler {
-    /// Associated event input message type
+    /// Associated input event type
     type Ein: 'static;
-    /// Associated event output message type
+    /// Associated output event type
     type Eout: 'static;
-    /// Associated read input message type
+    /// Associated input read type
     type Rin: 'static;
-    /// Associated read output message type
+    /// Associated output read type
     type Rout: 'static;
-    /// Associated write input message type
+    /// Associated input write type
     type Win: 'static;
-    /// Associated write output message type for
+    /// Associated output write type
     type Wout: 'static;
+    /// Associated result error type
+    type Error: 'static;
 
     /// Handles Rin and returns Rout for next inbound handler handling
-    fn handle_read(&mut self, msg: Self::Rin) -> Result<()>;
+    fn handle_read(&mut self, msg: Self::Rin) -> Result<(), Self::Error>;
 
     /// Polls Rout from internal queue for next inbound handler handling
     fn poll_read(&mut self) -> Option<Self::Rout>;
 
     /// Handles Win and returns Wout for next outbound handler handling
-    fn handle_write(&mut self, msg: Self::Win) -> Result<()>;
+    fn handle_write(&mut self, msg: Self::Win) -> Result<(), Self::Error>;
 
     /// Polls Wout from internal queue for next outbound handler handling
     fn poll_write(&mut self) -> Option<Self::Wout>;
 
     /// Handles event
-    fn handle_event(&mut self, _evt: Self::Ein) -> Result<()> {
+    fn handle_event(&mut self, _evt: Self::Ein) -> Result<(), Self::Error> {
         Ok(())
     }
 
@@ -38,7 +39,7 @@ pub trait Handler {
     }
 
     /// Handles timeout
-    fn handle_timeout(&mut self, _now: Instant) -> Result<()> {
+    fn handle_timeout(&mut self, _now: Instant) -> Result<(), Self::Error> {
         Ok(())
     }
 
